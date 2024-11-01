e46cbfa02f78c06fa5f3ed5a8417afaf0ce6a3e6
ilhamSuandi <ilham15suandi@gmail.com>
2024-11-01 22:25:22 +0700

    fix: change hard coded connection in test file

--- test/init.go
+++ test/init.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"github.com/ilhamSuandi/business_assistant/config"
 	"github.com/ilhamSuandi/business_assistant/database"
 	"github.com/ilhamSuandi/business_assistant/utils"
 	"gorm.io/gorm"
@@ -11,7 +12,7 @@ var DB *gorm.DB
 func init() {
 	utils.Log.Info("Initializing test environment")
 
-	db, err := database.Connect("localhost", "test_db")
+	db, err := database.Connect(config.DB_HOST, config.DB_USER)
 	if err != nil {
 		panic("failed to connect to database")
 	}
