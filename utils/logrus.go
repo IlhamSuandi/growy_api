b95540f5e192ff1a43ac616fd47da84531d87317
ilhamSuandi <ilham15suandi@gmail.com>
2025-01-16 17:14:02 +0700

    style(logger): change configuration for logging

--- utils/logrus.go
+++ utils/logrus.go
@@ -17,22 +17,13 @@ var (
 
 func init() {
 	Log = logrus.New()
-	LogJson = logrus.New()
 
-	// Set logger to use the custom text formatter
 	Log.SetFormatter(
 		&logrus.TextFormatter{
-			TimestampFormat: "2006/01/02 15:04:00",
+			TimestampFormat: "2006/01/02 15:04",
 			FullTimestamp:   true,
 			ForceColors:     true,
 			PadLevelText:    true,
-		},
-	)
-
-	LogJson.SetFormatter(
-		&logrus.JSONFormatter{
-			TimestampFormat: "2006/01/02 15:04:00",
-			PrettyPrint:     true,
 		},
 	)
 
